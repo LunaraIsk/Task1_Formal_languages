3c93728c165729899cd6de9c78fb45664e7c1c88
iskalievalunara0 <iskalievalunara0@gmail.com>
2020-11-26 18:22:00 +0300

    Добавьте файлы проекта.

---
+++ Task1_Formal_languages/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task1_Formal_languages
+{
+    class Program
+    {
+        static public string Input(string fileName)
+        {
+            StringBuilder result = new StringBuilder();
+
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                result.Append(file.ReadToEnd());
+            }
+
+            return result.ToString();
+        }
+        static void Main(string[] args)
+        {
+            FiniteStateMachine machine = new FiniteStateMachine("input_json.json");
+
+            string input = Input("inputString.txt");
+            int skip = 0;
+            while (skip < input.Length)
+            {
+                KeyValuePair<bool, int> result = machine.MaxString(input, skip);
+
+                if (result.Key == true)
+                {
+                    string output = input.Substring(skip, result.Value);
+
+                    using (StreamWriter file = new StreamWriter("output.txt", true, Encoding.Default))
+                    {
+                        file.WriteLine(output);
+                    }
+                    
+                    Console.WriteLine(output);
+                    skip += result.Value;
+                }
+                else
+                {
+                    skip++;
+                }
+            }
+        }
+    }
+}
